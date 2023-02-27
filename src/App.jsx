c2146958f2464e767bc90b301bb3852541a96ffa
Christian Medinilla <cris.medinill@gmail.com>
2023-02-27 10:20:45 -0600

    Add Actions and Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,11 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://github.com" target="_blank">
+          <img src="/github.png" className="logo Git Hub" alt="React logo" />
+        </a>
       </div>
-      <h1>Vite + React + GH</h1>
+      <h1>Vite + React + GH Actions + GH Pages</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
